127484025a05014b361f14656a729095f022c547
BSM <BlasSoriano@gmail.com>
2017-12-18 18:34:54 +0100

    Starting ParaworldResources DLL

---
+++ Paraworld/ParaworldResources/Graphics/BoundingBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaworldResources.Graphics
+{
+    public class BoundingBox
+    {
+        public Vertex min;
+        public Vertex max;
+
+        public BoundingBox() : this(new Vertex(), new Vertex()) { }
+
+        public BoundingBox(Vertex min, Vertex max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public bool Write(BinaryWriter bw)
+        {
+            if (!min.Write(bw)) return false;
+            if (!max.Write(bw)) return false;
+            return true;
+        }
+
+        public bool Read(BinaryReader br)
+        {
+            min = new Vertex();
+            max = new Vertex();
+            if (!min.Read(br)) return false;
+            if (!max.Read(br)) return false;
+            return true;
+        }
+
+        public static BoundingBox FromReader(BinaryReader br)
+        {
+            BoundingBox bb = new BoundingBox();
+            bb.Read(br);
+            return bb;
+        }
+    }
+}
---
+++ Paraworld/ParaworldResources/Graphics/Vertex.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaworldResources.Graphics
+{
+    public class Vertex
+    {
+        public const float MAX_VALUE_13BITS = 8191.0f;
+
+        public float X;
+        public float Y;
+        public float Z;
+
+        public Vertex() : this(0.0f, 0.0f, 0.0f) { }
+
+        public Vertex(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public bool Write(BinaryWriter bw)
+        {
+            bw.Write(X);
+            bw.Write(Y);
+            bw.Write(Z);
+            return true;
+        }
+
+        public bool Read(BinaryReader br)
+        {
+            X = br.ReadSingle();
+            Y = br.ReadSingle();
+            Z = br.ReadSingle();
+            return true;
+        }
+
+        public static Vertex FromReader(BinaryReader br)
+        {
+            Vertex v = new Vertex();
+            v.Read(br);
+            return v;
+        }
+
+        public static Vertex FromUInt64(ulong packedBits, BoundingBox boundingBox)
+        {
+            int ix, iy, iz;
+            float x, y, z;
+
+            ix = (int)((packedBits >> 0) & 0x1FFF);
+            iy = (int)((packedBits >> 13) & 0x1FFF);
+            iz = (int)((packedBits >> 26) & 0x1FFF);
+
+            x = ix * (boundingBox.max.X - boundingBox.min.X) / MAX_VALUE_13BITS + boundingBox.min.X;
+            y = iy * (boundingBox.max.Y - boundingBox.min.Y) / MAX_VALUE_13BITS + boundingBox.min.Y;
+            z = ix * (boundingBox.max.Z - boundingBox.min.Z) / MAX_VALUE_13BITS + boundingBox.min.Z;
+
+            return new Vertex(x, y, z);
+        }
+    }
+}
